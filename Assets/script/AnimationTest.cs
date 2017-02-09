74f5318d0aa898ff834995f7c7b1d7e16514bf0b
mat2proen <mashu@mn.cis.iwate-u.ac.jp>
2017-02-10 04:00:13 +0900

    2017_02_10_Animation
    cache Making

---
+++ Assets/script/AnimationTest.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class AnimationTest : MonoBehaviour {
+
+    const string clipDirectory = "Animation/";
+    const string clipName = "Falling";
+    Animation anim;
+    AnimationClip clip;
+    [SerializeField] int tick = 0;
+    
+	// Use this for initialization
+	void Start () {
+        clip = (AnimationClip)AnimationClip.Instantiate(Resources.Load(clipDirectory+clipName));
+        anim = GetComponent<Animation>();
+        anim.AddClip(clip, clipName);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (tick++ % 200 == 0)
+        {
+            anim.Play(clipName);
+        }
+        /*
+        if ((tick - 30) % 200 == 0)
+            anim.Stop();
+        */
+    }
+}
--- Assets/script/PrefectureScript.cs
+++ Assets/script/PrefectureScript.cs
@@ -10,6 +10,14 @@ public static class DefineGeneralImage
     public const float INIT_ALPHA = 1f;
     public const float INIT_TEXT_OFFSET_X = 0f;
     public const float INIT_TEXT_OFFSET_Y = -20f;
+    public const string PATH_ANIMATION = "Animation/";
+}
+
+public static class DefinePrefectureScript
+{
+    public const string PATH_CSV_FILE = "CSV/prefectureData";
+    public const string PATH_PREFECTURE_IMAGE = "Texture/Prefecture/Image/";
+    public const string PATH_PREFECTURE_TEXT = "Texture/Prefecture/Text/";
 }
 
 
@@ -20,13 +28,39 @@ public class PrefectureScript : MonoBehaviour {
 
     public GeneralImage puzzle;
     int tick = 0;
+    
+    /* Resource */
+    public class ResourceStore <T> where T : class{
+        Dictionary<string, T> resourceTable = new Dictionary<string, T>();
 
-
+        public T get(string path) {
+            //if (resourceTable[path] == null) {
+            T value;
+            if (!resourceTable.TryGetValue(path, out value)) {
+                resourceTable.Add(path, Resources.Load(path) as T);
+                //Debug.Log("hoge");
+            }
+            //Debug.Log(resourceTable.Keys);
+            resourceTable.TryGetValue(path, out value);
+            //Debug.Log(value);
+            //return value;
+            return resourceTable[path];
+            //return null;
+        }
+    }
+    
     public class GeneralImage {
+        // components
+        public GameObject objP;
         public GameObject obj;
         public SpriteRenderer spriterenderer;
         public Transform transform;
+        public Animation animation;
+        // cache
+        //public ResourceStore<Sprite> spriteStore = new ResourceStore<Sprite>();
+        //public ResourceStore<Object> clipStore = new ResourceStore<Object>();
 
+        // parameter
         public string name {
             get {
                 return obj.name;
@@ -59,6 +93,8 @@ public class PrefectureScript : MonoBehaviour {
             set {
                 pathBack = value;
                 spriterenderer.sprite = Resources.Load(value, typeof(Sprite)) as Sprite;
+                //spriterenderer.sprite = spriteStore.get(value);
+                //Debug.Log(spriterenderer.sprite);
             }
         }
         public float alpha {
@@ -89,6 +125,7 @@ public class PrefectureScript : MonoBehaviour {
             }
         }
 
+        // constructor
         public GeneralImage(GameObject prefab,
                         Transform parent,
                         string path,
@@ -97,24 +134,45 @@ public class PrefectureScript : MonoBehaviour {
                         Vector2 scale,
                         int order)
         {
+            // setting parent ( prefab <-> objP <-> obj )
+            objP = new GameObject(name+"P");
             obj = (GameObject)Instantiate(prefab);
-            transform = obj.GetComponent<Transform>();
-            spriterenderer = obj.GetComponent<SpriteRenderer>();
+            Transform transObj = obj.GetComponent<Transform>();
+            transObj.SetParent(objP.transform, false);
+
+            transform = objP.GetComponent<Transform>();
             transform.SetParent(parent, false);
+            spriterenderer = obj.GetComponent<SpriteRenderer>();
+            animation = obj.GetComponent<Animation>();
+
             this.path = path;
             this.name = name;
             this.pos = pos;
             this.scale = scale;
+
+            transObj.SetParent(transform, false);
+            transObj.localPosition = Vector3.zero;
+            transObj.localRotation = Quaternion.Euler(Vector3.zero);
+            transObj.localScale = Vector3.one;
+
             alpha = DefineGeneralImage.INIT_ALPHA;
             deltaAlpha = 0f;
             this.order = order;
         }
-        
+
+        public void PlayAnimation(string pathAnimationClip) {
+            AnimationClip clip = (AnimationClip)AnimationClip.Instantiate(Resources.Load(DefineGeneralImage.PATH_ANIMATION));
+            //Object tmp = clipStore.get(DefineGeneralImage.PATH_ANIMATION);
+            //AnimationClip clip = (AnimationClip)AnimationClip.Instantiate(tmp);
+            animation.AddClip(clip, DefineGeneralImage.PATH_ANIMATION);
+            animation.Play(DefineGeneralImage.PATH_ANIMATION);
+        }
     }
 
     public class Prefecture {
         public GeneralImage image;
         public GeneralImage text;
+        public GeneralImage imageback;
 
         public Prefecture(GameObject prefab,
                           Transform parent,
@@ -138,6 +196,13 @@ public class PrefectureScript : MonoBehaviour {
                                     pos + new Vector2(DefineGeneralImage.INIT_TEXT_OFFSET_X, DefineGeneralImage.INIT_TEXT_OFFSET_Y),
                                     textScale,
                                     100);
+            imageback = new GeneralImage(prefab,
+                                         parent,
+                                         "Texture/tofu",
+                                         name+"tofu",
+                                         pos,
+                                         new Vector2(30f,30f),
+                                         10);
         }
     }
 
@@ -151,7 +216,9 @@ public class PrefectureScript : MonoBehaviour {
                             new Vector2(-0f, 0f),
                             new Vector2(110f, 110f),
                             0);
-        puzzle.spriterenderer.sortingOrder = 0;
+        puzzle.alpha = 0f;
+        puzzle.deltaAlpha = 0.002f;
+        puzzle.order = 0;
         initPrefecture();
 	}
 	
@@ -166,7 +233,7 @@ public class PrefectureScript : MonoBehaviour {
     
     void initPrefecture()
     {
-        TextAsset csv = Resources.Load("CSV/prefectureData") as TextAsset;
+        TextAsset csv = Resources.Load(DefinePrefectureScript.PATH_CSV_FILE) as TextAsset;
         StringReader reader = new StringReader(csv.text);
         Vector2 imageSize = new Vector2(3f, 3f);
         Vector2 textSize = new Vector2(30f, 30f);
@@ -181,23 +248,21 @@ public class PrefectureScript : MonoBehaviour {
             Prefecture tmp = new Prefecture(
                 prefab,
                 this.transform,
-                "Texture/Prefecture/Image/" + values[0] + "1",
-                "Texture/Prefecture/text/" + values[0],
+                DefinePrefectureScript.PATH_PREFECTURE_IMAGE + values[0] + "1",
+                DefinePrefectureScript.PATH_PREFECTURE_TEXT + values[0],
                 values[0],
                 new Vector2(int.Parse(values[1]), int.Parse(values[2])),
                 imageSize,
                 textSize
                 );
-
+            tmp.imageback.alpha = 0.5f;
             prefTable.Add(values[0], tmp);
         }
     }
     
     void test() {
-        Vector2 u = new Vector2(1f, 1f);
+        //prefTable["iwate"].image.pos += Vector2.right;
 
-        //puzzle.pos += u;
-        //puzzle.scale -= u;
         if (tick == 100)
         {
             //puzzle.path = "Texture/Prefecture/Image/saitama1";
